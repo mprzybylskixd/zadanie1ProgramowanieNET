5464366b5f3b06f456d051bc719bd85cf74a9b14
mprzybylskixd <misiek44940@o2.pl>
2023-05-26 13:00:46 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MyApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Application app = new Application();
+            MainWindow mainWindow = new MainWindow();
+            app.Run(mainWindow);
+        }
+    }
+}
